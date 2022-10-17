f0a580e03c7e8196d5e72db6ba76342005b94b9b
toyosi <toyosiola@yaoo.com>
2022-10-17 11:21:03 -0700

    modified static files directory and made migrations

--- student_management_project/student_management_project/settings.py
+++ student_management_project/student_management_project/settings.py
@@ -127,8 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # print(TEMPLATE_DIRS)
 # print(BASE_DIR)
-print(STATIC_ROOT)
-print(STATICFILES_DIRS)
+# print(STATIC_ROOT)
+# print(STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
